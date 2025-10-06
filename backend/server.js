da687501f21bf23638031accf2c5fb2dbc0a11e3
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-10-06 21:09:50 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+//app config
+const app=express()
+const port = process.env.PORT || 4000
+
+//middlewares
+app.use(express.json())
+app.use(cors()) //to connect frontend with backend
+
+//api end point
+app.get('/',(req,res)=>{
+    res.send('API working')
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+    
+})
