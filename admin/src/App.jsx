60298631faf50a773b1eebfc44e76b69a0ecdcf6
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-10-11 13:37:07 +0530

    designed sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,12 @@ import Login from './pages/Login'
  import { ToastContainer, toast } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import DoctorsList from './pages/Admin/DoctorsList';
+import AddDoctor from './pages/Admin/AddDoctor';
 
 const App = () => {
 
@@ -12,6 +18,20 @@ const App = () => {
     <div className='bg-[#F8F9FD]'>
       <ToastContainer/>
       <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar/>
+        <Routes>
+          {/* Not displaying */}
+          <Route path='/' element={<></>}/> 
+          <Route path='/admin-dashboard' element={<Dashboard/>}/> 
+          <Route path='/all-appointments' element={<AllAppointments/>}/>
+          <Route path='/add-doctor' element={<AddDoctor/>}/>
+          <Route path='/doctor-list' element={<DoctorsList/>}/>
+
+
+
+        </Routes>
+      </div>
 
     </div>
   ) : (
