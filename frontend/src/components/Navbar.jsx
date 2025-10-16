6611bf99988ed6e1aad86195fe640550ba9ebf53
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-10-16 17:21:26 +0530

    ui changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -45,9 +45,9 @@ const logout=()=>{
                         <img className='w-2.5' src={assets.dropdown_icon} alt="" />
                         <div className={`absolute top-0 right-0 pt-14 text-base font-medium text-gray-600   ${showDropdown ? 'block' : 'hidden'}  `}>
                             <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-                                <p onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
-                                <p onClick={()=>navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
-                                <p onClick={logout} className='hover:text-black cursor-pointer'>Logout</p>
+                                <button onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</button>
+                                <button onClick={()=>navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</button>
+                                <button onClick={logout} className='hover:text-black cursor-pointer'>Logout</button>
                             </div>
                         </div>
                     </div>
