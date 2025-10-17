8e0ec74753f69b0d38f51bbfc049dd374d50cdda
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-10-17 21:27:05 +0530

    doctor appointment backend + frontend

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,19 +1,25 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets';
 import RelatedDoctors from '../components/RelatedDoctors';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const Appointment = () => {
 
   const { docId } = useParams();
-  const { doctors,currencySymbol } = useContext(AppContext);
+  const { doctors,currencySymbol , backendUrl, token, getDoctorsData } = useContext(AppContext);
   const daysOfWeek=['SUN','MON','TUE','WED','THU','FRI','SAT']
+
+  const navigate=useNavigate()
 
   const [docInfo, setDocInfo] = useState(null);
   const [docSlots,setDocSlots]=useState([])
   const [slotIndex,setSlotIndex]=useState(0);
   const [slotTime,setSlotTime]=useState('')
+
+
 
 
 
@@ -64,6 +70,35 @@ const Appointment = () => {
       }
       setDocSlots((prev) => [...prev, timeSlots]);
     }
+  }
+
+  const bookAppointment=async () => {
+    if(!token){
+      toast.warn('Login to book appointment')
+      return navigate('/login')
+    }
+    try {
+      const date=docSlots[slotIndex][0].dateTime
+
+      let day=date.getDate()
+      let month=date.getMonth()+1
+      let year=date.getFullYear()
+
+      const slotDate=day+"_"+month+"_"+year
+      const {data}=await axios.post(backendUrl+'/api/user/book-appointment',{docId,slotDate,slotTime},{headers:{token}})
+
+      if(data.success){
+        toast.success(data.message)
+        getDoctorsData()
+        navigate('/my-appointments')
+      }else{
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+    
   }
 
   useEffect(() => {
@@ -131,7 +166,7 @@ const Appointment = () => {
               </p>
             ))}
           </div>
-            <button className='bg-[#5f6FFF] text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
+            <button onClick={bookAppointment} className='bg-[#5f6FFF] text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
       </div>
       {/* Related doctors */}
       <RelatedDoctors docId={docId} speciality={docInfo.speciality}/>
