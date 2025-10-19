c99bdac762d6c789973ea3772d736bb5ede496e2
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-10-19 07:35:09 +0530

    my appointment page frontend+backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { bookAppointment, getProfile, listAppointment, loginUser, registerUser, updateProfile } from '../controllers/userController.js'
+import { bookAppointment, cancelAppointment, getProfile, listAppointment, loginUser, registerUser, updateProfile } from '../controllers/userController.js'
 import authUser from '../middleware/authUser.js'
 import upload from '../middleware/multer.js'
 
@@ -14,5 +14,6 @@ userRouter.post('/book-appointment',authUser,bookAppointment)
 
 userRouter.get('/appointment',authUser,listAppointment)
 
+userRouter.post('/cancel-appointment',authUser,cancelAppointment)
 
 export default userRouter
