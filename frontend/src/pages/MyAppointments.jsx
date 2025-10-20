836376330b7813fa6989d49f39f7a514eea1af51
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-10-20 09:31:30 +0530

    fixed bug

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { useEffect } from 'react'
-import useNavigate from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 const MyAppointments = () => {
   const {backendUrl,token ,getDoctorsData}=useContext(AppContext)
