5fbb27e7b5c05cf62fe7c662a0c66199d4eea929
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-10-21 18:29:43 +0530

    doctor profile page and appointment page

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -11,6 +11,8 @@ const DoctorContextProvider=(props)=>{
 
     const[dToken,setDToken]=useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
     const[appointments,setAppointments]=useState([])
+    const[dashData,setDashData]=useState(false)
+    const [profileData,setProfileData]=useState(false)
 
     const getAppointments=async () => {
         try {
@@ -57,12 +59,51 @@ const DoctorContextProvider=(props)=>{
 
     }
 
+    const getDashData=async () => {
+        try {
+            const {data}=await axios.get(backendUrl+'/api/doctor/dashboard',{headers:{dToken}})
+            if(data.success){
+                setDashData(data.dashData)
+                console.log(data.dashData);
+                
+            }else{
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+            
+        }
+        
+    }
+
+    const getProfileData=async (req,res) => {
+        try {
+
+            const {data}=await axios.get(backendUrl+'/api/doctor/profile',{headers:{dToken}})
+            if(data.success){
+                setProfileData(data.profileData)
+                console.log(data.profileData);
+                
+            }else{
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+             toast.error(error.message)
+        }
+        
+    }
+
     
     const value={
         dToken,setDToken,
         backendUrl,
         appointments,setAppointments,getAppointments,
         completeAppointment,cancelAppointment,
+        dashData,setDashData,getDashData,
+        profileData,setProfileData,getProfileData,
+        
 
 
 
