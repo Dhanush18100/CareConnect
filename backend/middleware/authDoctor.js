7f6709ede8ec36ddd0edde204a62660060d96ca1
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-10-21 14:10:05 +0530

    Appointment page for doctor pannel

---
+++ backend/middleware/authDoctor.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken'
+
+//Doctor authentication middleware
+
+const authDoctor=async (req,res,next) => {
+    try {
+        const {dtoken}=req.headers
+        if(!dtoken){
+            return res.json({success:false,message:"Not authorized login again"})
+
+        }
+        const token_decode=jwt.verify(dtoken ,process.env.JWT_SECRETE)
+       if (!req.body) req.body = {};
+
+        req.body.docId=token_decode.id //we will get user id from token and we add token to body
+        
+        next()
+    } catch (error) {
+
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
+}
+
+export default authDoctor
